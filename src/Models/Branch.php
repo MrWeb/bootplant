e078fb5ed978743119bd5fb0aa2dcb317ca9b6f6
Mr.Web <lombardi.nd@gmail.com>
2019-03-14 08:40:06 +0100

    Branch removed scope che bloccava la visualizzazione

--- src/Models/Branch.php
+++ src/Models/Branch.php
@@ -50,26 +50,8 @@ class Branch extends Model
         return $this->hasMany(User::class);
     }
 
-    public function estates()
-    {
-        return $this->hasMany(Estate::class);
-    }
-
     public function meta()
     {
         return $this->hasMany(Meta::class);
-    }
-
-    /**
-     * Tiene lo scope del branch in automatico in base all'utente
-     * Da implementare per i super-admin una volta fatto il login
-     */
-    protected static function boot()
-    {
-        parent::boot();
-        // Punta sempre e solo al branch_id dell'utente
-        static::addGlobalScope('id', function (Builder $builder) {
-            $builder->where('id', '=', 2); //2 va edited
-        });
     }
 }
