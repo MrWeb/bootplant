18a9d9dd8f56ed1e3d26f156c22c8e0dbae4d874
Mr.Web <lombardi.nd@gmail.com>
2019-03-18 08:33:59 +0100

    Tab vue directive from php

--- src/resources/views/components/tabs/tab.blade.php
+++ src/resources/views/components/tabs/tab.blade.php
@@ -1,1 +1,1 @@
-    <a class="nav-item nav-link{{isset($active) ? ' active' : ''}}" data-toggle="tab" href="#{{$tab}}" role="tab" aria-controls="{{$tab}}" aria-selected="false">{{$tab}}</a>
+<a class="nav-item nav-link{{isset($active) ? ' active' : ''}}" {{isset($vue) ? $vue : ''}} data-toggle="tab" href="#{{$tab}}" role="tab" aria-controls="{{$tab}}" aria-selected="false">{{$tab}}</a>
